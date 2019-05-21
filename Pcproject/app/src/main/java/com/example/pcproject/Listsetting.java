ea5c6a5c50ffddc4ec5098b2a935e9338002f983
PcProject <cis8448@naver.com>
2019-05-21 09:49:15 +0900

    no message

---
+++ Pcproject/app/src/main/java/com/example/pcproject/Listsetting.java
@@ -0,0 +1,67 @@
+package com.example.pcproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Listsetting {
+
+    ArrayList<unmemberbean> allmember;
+
+    public Listsetting(ArrayList<unmemberbean> allmember){
+        this.allmember = allmember;
+    }
+
+    public Adapter memberListSetting(){
+        Adapter memberAdapter = null;
+
+
+        return memberAdapter;
+    }
+
+
+ public class MemberAdapterSet extends BaseAdapter{
+        ArrayList<Memberbeen> allmember;
+        public MemberAdapterSet(ArrayList allmem){
+            this.allmember = allmem;
+        }
+     @Override
+     public int getCount() {
+         return allmember.size();
+     }
+
+     @Override
+     public Object getItem(int position) {
+         return allmember.get(position) ;
+     }
+
+     @Override
+     public long getItemId(int position) {
+         return position;
+     }
+
+     @Override
+     public View getView(int position, View convertView, ViewGroup parent) {
+         final int pos = position;
+         final Context context = parent.getContext();
+         if(convertView == null){
+             LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+             convertView = inflater.inflate(R.layout.membermanagment2,parent,false);
+         }
+         TextView nametxt, birthtxt, idtxt, timetxt;
+
+         nametxt = convertView.findViewById(R.id.mName);
+         birthtxt = convertView.findViewById(R.id.mDb);
+         idtxt = convertView.findViewById(R.id.mId);
+         timetxt = convertView.findViewById(R.id.mTime);
+         for(int i = 0 ; i < allmember.size();)
+         return convertView;
+     }
+ }
+}
--- Pcproject/app/src/main/java/com/example/pcproject/membermanagment.java
+++ Pcproject/app/src/main/java/com/example/pcproject/membermanagment.java
@@ -12,9 +12,15 @@ public class membermanagment  extends AppCompatActivity {
     ListView list1;
     ArrayList<Memberbeen> memberlist;
     Adapter memberAdapter;
+    Controller con = Controller.getInstance();
+    final String LISTSET = "listset";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.membermanagment);
+        list1 = findViewById(R.id.mList);
+        if(memberAdapter == null ){
+            con.sub(membermanagment.this,LISTSET);
+        }
 
     }
 }
